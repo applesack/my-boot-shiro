1be45045299d65f1dd123bd49133dc103d8ad091
applesack <2383955734@qq.com>
2020-12-03 19:15:55 +0800

    commit first

---
+++ bootshiro/src/test/java/xyz/scootaloo/bootshiro/BootshiroApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.scootaloo.bootshiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootshiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
